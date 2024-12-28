a8aa28db53faedf658f4b393197206943bc42215
vishal-gohil12 <21it039@charusat.edu.in>
2024-12-28 18:36:28 +0530

    add defult port

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { projectRoute } from "./routes/projectRoute";
 
 config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
